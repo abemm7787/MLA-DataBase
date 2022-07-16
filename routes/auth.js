938c02e67bdf24480f56524aceff422fbbb3b382
Abraham Mirambeaux <mirambeaux7@gmail.com>
2022-07-15 20:48:55 -0400

    added a encryption to hide password

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,26 @@
 const router = require("express").Router()
 const User = require("../modals/User")
-// const CryptoJS = require("crypto.js")
+const CryptoJS = require("crypto-js")
 
 
 
+//REGISTER
 router.post("/register", async (req, res) => {
-
     const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-
-    })
-
-
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
+    });
+  
     try {
-        const savedUser = await newUser.save()
-        res.status(201).json(savedUser)
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    catch (err) {
-        res.status(500).json(err)
-        // console.log(err)
-    }
-})
+  });
 
 module.exports = router
